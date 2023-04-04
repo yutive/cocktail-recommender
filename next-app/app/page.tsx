a328dfa969f57d329a53044d1bcc3a5339ba9418
laurin <laurin.schwander@outlook.com>
2023-04-04 11:42:43 +0200

    added two more cocktails

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,26 +1,70 @@
+import Link from "next/link";
+
 export default async function Home() {
-    const data = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate:0}})
+
+    const data = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate: 0}})
     const res = await data.json()
+    const data1 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate: 0}})
+    const res1 = await data1.json()
+    const data2 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate: 0}})
+    const res2 = await data2.json()
+
+
+
+    // res.drinks[0].strDrinkThumb
+    // res.drinks[0].strAlcoholic
+    // res.drinks[0].strInstructionsDE
+    // res.drinks[0].idDrink
+
 
     return (
-        <main className="flex flex-col items-center justify-center h-screen">
-            <div className="max-w-md mx-10 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src={res.drinks[0].strDrinkThumb}
-                             alt="Modern building architecture"/>
+        <main>
+            <div className="flex flex-col lg:flex-row items-center justify-center lg:h-screen">
+                <Link href={`/${res.drinks[0].idDrink}`}>
+                    <div className="rounded-2xl shadow-lg bg-neutral-700 m-10 mt-28 lg:mt-10 hover:shadow-2xl">
+                        <img className="h-1/3 w-full rounded-2xl object-cover" src={res.drinks[0].strDrinkThumb}
+                             alt="Mountain"/>
+                        <div className="p-8">
+                            <div
+                                className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{res.drinks[0].strAlcoholic}
+                            </div>
+                            <p
+                               className="text-xl mt-1 font-medium leading-tight text-white hover:underline">{res.drinks[0].strDrink}
+                            </p>
+                        </div>
                     </div>
-                <div className="p-8 bg-neutral-700">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                        {res.drinks[0].strAlcoholic}
+                </Link>
+                <Link href={`/${res1.drinks[0].idDrink}`}>
+                    <div className="rounded-2xl shadow-lg bg-neutral-700 m-10 mt-10 hover:shadow-2xl">
+                        <img className="h-1/3 w-full rounded-2xl object-cover" src={res1.drinks[0].strDrinkThumb}
+                             alt="Mountain"/>
+                        <div className="p-8">
+                            <div
+                                className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{res1.drinks[0].strAlcoholic}
+                            </div>
+                            <p
+                               className="text-xl mt-1 font-medium leading-tight text-white hover:underline">{res1.drinks[0].strDrink}
+                            </p>
+                        </div>
                     </div>
-                    <a href="#" className="block mt-1 text-lg text-white leading-tight font-medium text-black hover:underline">{res.drinks[0].strDrink}</a>
-                    <p className="mt-2 text-slate-200">
-                        {res.drinks[0].strInstructionsDE}
-                    </p>
-                </div>
-              </div>
+                </Link>
+                <Link href={`/${res2.drinks[0].idDrink}`}>
+                    <div className="rounded-2xl shadow-lg bg-neutral-700 m-10 mt-10 hover:shadow-2xl">
+                        <img className="h-1/3 w-full rounded-2xl object-cover" src={res2.drinks[0].strDrinkThumb}
+                             alt="Mountain"/>
+                        <div className="p-8">
+                            <div
+                                className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{res2.drinks[0].strAlcoholic}
+                            </div>
+                            <p
+                               className="text-xl mt-1 font-medium leading-tight text-white hover:underline">{res2.drinks[0].strDrink}
+                            </p>
+                        </div>
+                    </div>
+                </Link>
             </div>
+
         </main>
+
     )
 }
