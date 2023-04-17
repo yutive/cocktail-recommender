b16d4b140eea399e9f43ee88f9a138118c7d1fb5
laurin <laurin.schwander@outlook.com>
2023-04-17 17:11:12 +0200

    implemented node.js api service

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch")
+const express = require("express");
+const app = express();
+const port = 3080;
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.get('/drinks', async (req, res) => {
+    const drink0 = await getCocktail()
+    const drink1 = await getCocktail()
+    const drink2 = await getCocktail()
+    const result = {
+        drinks: []
+    };
+    result.drinks = drink0.drinks.concat(drink1.drinks).concat(drink2.drinks)
+    res.json(result);
+});
+app.listen(port, function (err) {
+    if (err) console.log(err);
+    console.log(`API is running at at http://localhost:${port}/drinks`);
+});
+
+async function getCocktail(){
+    const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+    const data = await response.json()
+    return await data
+}
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -2,13 +2,8 @@ import Link from "next/link";
 
 export default async function Home() {
 
-    const data = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate: 0}})
+    const data = await fetch('http://localhost:3080/drinks', {next: {revalidate: 0}})
     const res = await data.json()
-    const data1 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate: 0}})
-    const res1 = await data1.json()
-    const data2 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {next: {revalidate: 0}})
-    const res2 = await data2.json()
-
 
     // res.drinks[0].strDrinkThumb
     // res.drinks[0].strAlcoholic
@@ -33,30 +28,30 @@ export default async function Home() {
                         </div>
                     </div>
                 </Link>
-                <Link href={`/${res1.drinks[0].idDrink}`}>
+                <Link href={`/${res.drinks[1].idDrink}`}>
                     <div
                         className="transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-105 duration-300 max-w-md mx-auto lg:mx-10 overflow-hidden md:max-w-2xl rounded-2xl shadow-lg bg-neutral-700 m-10 hover:shadow-2xl">
-                        <img className="rounded-2xl object-cover" alt="Cocktail" src={res1.drinks[0].strDrinkThumb}/>
+                        <img className="rounded-2xl object-cover" alt="Cocktail" src={res.drinks[1].strDrinkThumb}/>
                         <div className="p-8">
                             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                                {res1.drinks[0].strAlcoholic}
+                                {res.drinks[1].strAlcoholic}
                             </div>
                             <p className="text-xl mt-1 font-medium leading-tight text-white">
-                                {res1.drinks[0].strDrink}
+                                {res.drinks[1].strDrink}
                             </p>
                         </div>
                     </div>
                 </Link>
-                <Link href={`/${res2.drinks[0].idDrink}`}>
+                <Link href={`/${res.drinks[2].idDrink}`}>
                     <div
                         className="transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-105 duration-300 max-w-md mx-auto lg:mx-10 overflow-hidden md:max-w-2xl rounded-2xl shadow-lg bg-neutral-700 m-10 hover:shadow-2xl">
-                        <img className="rounded-2xl object-cover" alt="Cocktail" src={res2.drinks[0].strDrinkThumb}/>
+                        <img className="rounded-2xl object-cover" alt="Cocktail" src={res.drinks[2].strDrinkThumb}/>
                         <div className="p-8">
                             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                                {res2.drinks[0].strAlcoholic}
+                                {res.drinks[2].strAlcoholic}
                             </div>
                             <p className="text-xl mt-1 font-medium leading-tight text-white">
-                                {res2.drinks[0].strDrink}
+                                {res.drinks[2].strDrink}
                             </p>
                         </div>
                     </div>
